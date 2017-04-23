d60204b6b7d8ccb5cb344f552e841f4c4403abf8
Logan May <logsta.may@gmail.com>
2017-04-23 11:20:24 -0400

    lessons

--- index.js
+++ index.js
@@ -10,11 +10,32 @@ app.set('port', (process.env.PORT || 5000));
 
 app.post('/message', function(req, res) {
   var twilio = require('twilio');
-  console.log(req.body.Body);
-  var twiml = new twilio.TwimlResponse();
-  twiml.message('The Robots are coming! Head for the hills!');
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
+  if(req.body.Body == 'Algebra') {
+	  var twiml = new twilio.TwimlResponse();
+	  twiml.message("Welcome to SMSLearn - Algebra! Let's get started!  What's 4 + 4? (Please respond in the form 4+4=?)");
+	  res.writeHead(200, {'Content-Type': 'text/xml'});
+	  res.end(twiml.toString());
+	} else if(req.body.Body == '4+4=8') {
+	  var twiml = new twilio.TwimlResponse();
+	  twiml.message("Nice work! Here's another. What's 4 * 9? (Please respond in the form 4*9=?)");
+	  res.writeHead(200, {'Content-Type': 'text/xml'});
+	  res.end(twiml.toString());
+	} else if(req.body.Body == '4*9=36') {
+	  var twiml = new twilio.TwimlResponse();
+	  twiml.message("Owwiee! You hot! What's 14 / 2? (Please respond in the form 14/2=?)");
+	  res.writeHead(200, {'Content-Type': 'text/xml'});
+	  res.end(twiml.toString());
+	} else if(req.body.Body == '14/2=7') {
+	  var twiml = new twilio.TwimlResponse();
+	  twiml.message("Get.. get.. get it poppin'! What's 104 / 2? (Please respond in the form 104/2=?)");
+	  res.writeHead(200, {'Content-Type': 'text/xml'});
+	  res.end(twiml.toString());
+	} else if(req.body.Body == '104/2=52') {
+	  var twiml = new twilio.TwimlResponse();
+	  twiml.message("Oh my good golly! You are ready to move on! Nice job ;)");
+	  res.writeHead(200, {'Content-Type': 'text/xml'});
+	  res.end(twiml.toString());
+	}
 });
 
 http.createServer(app).listen(app.get('port'), function () {
